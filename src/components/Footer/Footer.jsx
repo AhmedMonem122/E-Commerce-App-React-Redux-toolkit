30ba0878719c4a2660981c153c3433fe042d068a
AhmedMonem122 <ahmed.monem90@yahoo.com>
2023-06-19 22:25:34 +0300

    E-Commerce App

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto">
+        <p className="text-white text-center">
+          Copyright &copy; Ecommerce Shop 2023 by Ahmed Monem.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
